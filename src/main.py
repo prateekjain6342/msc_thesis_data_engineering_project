6301fb0f7a24bbd1a9423a578103adc259eb40b3
Prateek <prateekjain6342@gmail.com>
2024-03-10 15:58:10 +0530

    Added NormaliseOrders

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import json
-from utils import NormaliseIoT
+from utils import NormaliseIoT, NormaliseOrders
 
 def get_iot_data():
     data = json.load(open('./data/iot_data.json', 'r'))
+    return data
+
+def get_orders_data():
+    data = pd.read_csv('./data/orders_data.csv')
     return data
 
 if __name__ == "__main__":
@@ -21,3 +25,16 @@ if __name__ == "__main__":
     # Converting Data to Pandas Dataframe for further processing
     iot_data = pd.json_normalize(normalised_data)
     print(iot_data)
+
+    orders_data = get_orders_data()
+    
+    normalised_orders = list()
+    for data in orders_data.to_dict('records'):
+        tmp_data = NormaliseOrders(data).build()
+        # Adding pseudo vehicle id for combining
+        tmp_data['vehicle_id'] = 1
+        normalised_orders += [tmp_data]
+    
+    # Converting Data back to Pandas Dataframe for further processing
+    orders_data = pd.json_normalize(normalised_orders)
+    print(orders_data)
--- src/utils.py
+++ src/utils.py
@@ -105,3 +105,75 @@ class NormaliseIoT:
             attr: new_date_value
         }
     
+class NormaliseOrders:
+
+    def __init__(self, data: dict) -> None:
+        self.data = data
+    
+    def build(self):
+        switcher = {
+            "_build_delivery_address": self._build_delivery_address,
+            "delivery_status": self._build_delivery_status,
+            "delivery_time": self._build_delivery_time,
+            "delivery_cost": self._build_delivery_cost,
+            "recipient_name": self._build_recipient_name,
+            "order_date": self._build_order_date
+        }
+
+        new_data = dict()
+        for attr in switcher.keys():
+            new_data.update(switcher[attr](attr))
+        
+        return new_data
+    
+    def _build_delivery_address(self, attr):
+        """
+        Method to build the delivery_address field using the delivery_address field
+        """
+        return {
+            attr: self.data.get("delivery_address", "")
+        }
+
+    def _build_delivery_status(self, attr):
+        """
+        Method to build the delivery_status field using the delivery_status field
+        """
+        return {
+            attr: self.data.get("delivery_status", "")
+        }
+    
+    def _build_delivery_time(self, attr):
+        """
+        Method to build the delivery_time field using the delivery_time field
+        """
+        return {
+            attr: self.data.get("delivery_time", "")
+        }
+    
+    def _build_delivery_cost(self, attr):
+        """
+        Method to build the delivery_cost field using the delivery_cost field
+        """
+        return {
+            attr: self.data.get("delivery_cost", "")
+        }
+    
+    def _build_recipient_name(self, attr):
+        """
+        Method to build the recipient_name field using the recipient_name field
+        """
+        return {
+            attr: self.data.get("recipient_name", "")
+        }
+
+    def _build_order_date(self, attr):
+        """
+        Method to build the order_date field using the order_date field
+        Expected input format for order_date: mm/dd/yyyy
+        """
+
+        new_date_value = datetime.datetime.strptime(str(self.data.get("order_date")), "%m/%d/%Y").date()
+
+        return {
+            attr: new_date_value
+        }
